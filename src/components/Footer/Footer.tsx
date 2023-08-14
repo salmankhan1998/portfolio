1699bd3014810976a37ba86c69f5c47dee3bb559
salmankhan1998 <skhandilshad1998@gamil.com>
2023-08-14 15:16:55 -0400

    about description changed and footer links updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,11 +15,11 @@ export function Footer() {
       </div>
 
       <div className="social-media">
-        <a href="#" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/salman-khan-26008520a" target="_blank" rel="noreferrer">
           <img src={linkedinIcon} alt="Linkedin" />
         </a>
 
-        <a href="#" target="_blank" rel="noreferrer">
+        <a href="https://github.com/salmankhan1998" target="_blank" rel="noreferrer">
           <img src={githubIcon} alt="GitHub" />
         </a>
 
